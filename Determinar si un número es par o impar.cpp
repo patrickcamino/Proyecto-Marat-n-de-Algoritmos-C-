913dc0208cd5056b2fe3db75302d53d983909537
patrickcamino <pcamino@alumno.montepiedra.edu.ec>
2025-10-06 08:48:13 -0500

    Determinar si un número es par o impar

---
+++ Determinar si un número es par o impar.cpp
@@ -0,0 +1,10 @@
+//Calcular el área de un triángulo //
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingrese un numero: ";
+    cin >> n;
+    cout << (n % 2 == 0 ? "El numero es par." : "El numero es impar.");
+    return 0;
+}
