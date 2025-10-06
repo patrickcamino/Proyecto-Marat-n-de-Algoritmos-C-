676cb6010c7dbe3a9dcca74bcb0c698e7f45036f
patrickcamino <pcamino@alumno.montepiedra.edu.ec>
2025-10-06 08:40:13 -0500

    Suma de dos números

---
+++ Suma de dos números.cpp
@@ -0,0 +1,23 @@
+//Suma de dos números  //
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaración de variables
+    float num1, num2, suma;
+
+    // Entrada de datos
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    // Proceso
+    suma = num1 + num2;
+
+    // Salida
+    cout << "La suma de los dos números es: " << suma << endl;
+
+    return 0;
+}
